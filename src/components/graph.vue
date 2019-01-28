8a4b03500c46583711c367239284a805909a33df
Lance Owen Merrill <Lanceom@hawaii.edu>
2019-01-27 16:03:07 -0800

    graph works with dates as labels

--- src/components/graph.vue
+++ src/components/graph.vue
@@ -1,33 +1,50 @@
 <template>
-    <div >
-        <h3>This is the graph component</h3>
-        <p v-if="stats !== null">Accounts Created: {{stats.stats[0].accounts_created}} On {{stats.stats[0].date}}</p>
 
-    <div>
 
-    </div>
-    </div>
+    <div v-if="accounts.datasets[0].data">
+        <LineGraph :chartdata="accounts" />
+        </div>
 </template>
 
 <script>
 import RippleApi from '../services/rippleApi';
+import LineGraph from './LineGraph.vue';
 export default {
-    data() {
-        return {
-            stats: null
-        };
+    components: {
+        LineGraph
     },
-
-
+    data: () => ({
+        accounts: {
+            labels: null,
+            datasets: [
+                {
+                    label: 'Accounts Created',
+                    backgroundColor: '#f87979',
+                    data: null
+                }
+            ]
+        },
+        options: {
+            responsive: false,
+            maintainAspectRatio: false
+        },
+    }),
     created() {
-        const today = new Date();
-        console.log(today);
-
         RippleApi.getStats()
             .then(s => {
-                this.stats = s;
-
+                console.log('s', s);
+                this.accounts.datasets[0].data = s.stats.map(stat => {
+                    return stat.accounts_created;
+                });
+                this.accounts.labels = s.stats.map(stat => {
+                    return stat.date;
+                });
+                console.log('hello', this.accounts);
             });
+
+        
+        
+    
     }
 };
 </script>
